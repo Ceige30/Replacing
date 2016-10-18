c911e37f85bee14774ebbb629c922d81069fb87c
cm921 <sig91308@gmail.com>
2016-10-18 14:15:55 -0400

    Another one

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,6 +7,6 @@ public class Main {
         String b = a.replace('1', '2' );
         System.out.println(b);
         //This basicly replace all of one character with another in a string
-        //as you can see in "Tester 1 2 3" is repalces 1 with 2 so it now says "Tester 2 2 3
+        //as you can see in "Tester 1 2 3" is replaces 1 with 2 so it now says "Tester 2 2 3
     }
 }
