f8f63aa2569350d1ea55f47cc6d90ea34876883f
cm921 <sig91308@gmail.com>
2016-10-18 14:10:07 -0400

    Initial commit sorta

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String a = "Tester 1 2 3";
+        String b = a.replace('1', '2' );
+        System.out.println(b);
+        //This basicly replace all of one character with another in a string
+    }
+}
